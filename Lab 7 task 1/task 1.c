f96ee65edb50884eec0ce4cf4bd14d2b708f4d66
Kraken15th <ahmedkarrar79@gmail.com>
2025-04-05 19:46:27 +0200

    Add files via upload

---
+++ Lab 7 task 1/task 1.c
@@ -0,0 +1,32 @@
+main(){
+
+/*char kraken[] = {'K', 'r', 'a', 'k', 'e', 'n', '\0'}; //task3
+int i;
+for (i=0; i<6; i++){
+printf("%c",kraken[i]);
+}
+*/
+
+/*char kraken[] = " Kraken "; //task 4
+printf("%s",kraken);
+*/
+  /*char str1[30] = "Ahmed"; //task5
+  char str2[30] = "Karrar";
+  char str3[60];
+  strcat(str1, str2);
+  strcpy(str3, str1);
+
+
+  printf("%s\n", str3);
+  printf("%zu\n", strlen(str3)); //task 6
+  return 0;
+  */
+
+  /*int str3[60];
+  printf("%lu", sizeof(str3)); //task 7
+*/
+
+
+
+
+}
